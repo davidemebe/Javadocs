96129fad763a1e0bf41a1d8806223c812b943ec9
davidemebe <dmartinezbarreiro@danielcastelao.org>
2023-11-20 09:33:38 +0100

    ej1

---
+++ src/Ejercicio01.java
@@ -0,0 +1,12 @@
+public class Ejercicio01 {
+    int contador = 0;
+
+    public void frasebucle() {
+        do {
+            System.out.println("isto e un bucle");
+            contador++;
+        } while (contador < 5);
+
+    }
+}
+
