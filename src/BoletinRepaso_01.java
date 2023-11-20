4ad0e89cd0b431284fd0f97886bbbe47ca1c13ad
davidemebe <dmartinezbarreiro@danielcastelao.org>
2023-11-20 09:16:02 +0100

    doc

---
+++ src/BoletinRepaso_01.java
@@ -0,0 +1,20 @@
+
+public class BoletinRepaso_01 {
+
+    public static void main(String[] args) {
+        //ejercicio1
+        Ejercicio01 bucle1 = new Ejercicio01();
+        bucle1.frasebucle();
+        //ejercicio2
+        //Ejercicio02 bucle2 = new Ejercicio02();
+        //bucle2.numeros5();
+        //ejercicio3
+        // Ejercicio03 bucle3 = new Ejercicio03();
+        // bucle3.preme0();
+        //Ejercicio4
+        //Ejercicio4 bucle4 = new Ejercicio4();
+        //bucle4.numeros4();
+        //Ejercicio05 bucle5 = new Ejercicio05();
+        //bucle5.sumar5();
+    }
+}
